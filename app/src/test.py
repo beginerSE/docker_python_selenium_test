c6cb80abb34796f10cc96acfb9dfa31b8a1722e9
ミナピピン <42573936+beginerSE@users.noreply.github.com>
2023-10-24 18:50:12 +0900

    SELENIUM4 用にコードを修正

--- app/src/test.py
+++ app/src/test.py
@@ -1,17 +1,27 @@
 import os
-
+from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import selenium
+
+print("seleniumのバージョン",selenium.__version__)
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+options = FirefoxOptions()
+cloud_options = {}
+cloud_options['build'] = "build_1"
+cloud_options['name'] = "test_abc"
+options.set_capability('cloud:options', cloud_options)
 
 
 driver = webdriver.Remote(
     command_executor=os.environ["SELENIUM_URL"],
-    desired_capabilities=DesiredCapabilities.FIREFOX.copy()
+    options=options
 )
 driver.implicitly_wait(5)
 
 # ここからコーディング
 
 driver.get("https://www.time-j.net/worldtime/country/jp")
-print(driver.find_element_by_xpath("/html/body/div/div[6]/div[1]/div/p[5]").text)
+
+print(driver.find_element(By.XPATH, "/html/body/div/div[6]/div[1]/div/p[5]").text)
 driver.quit()
